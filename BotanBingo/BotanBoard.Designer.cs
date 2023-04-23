419da98be6bdfa8a28458c1b95f26293d7a26504
SyspectGlitch <32527907+SyspectGlitch@users.noreply.github.com>
2023-04-23 00:39:40 -0500

    Add files via upload

---
+++ BotanBingo/BotanBoard.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace BotanBingo
+{
+    partial class BotanBoard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FreeSpace = new System.Windows.Forms.Button();
+            this.space1 = new System.Windows.Forms.Button();
+            this.space2 = new System.Windows.Forms.Button();
+            this.space3 = new System.Windows.Forms.Button();
+            this.space4 = new System.Windows.Forms.Button();
+            this.space5 = new System.Windows.Forms.Button();
+            this.space6 = new System.Windows.Forms.Button();
+            this.space7 = new System.Windows.Forms.Button();
+            this.space8 = new System.Windows.Forms.Button();
+            this.space9 = new System.Windows.Forms.Button();
+            this.space10 = new System.Windows.Forms.Button();
+            this.space11 = new System.Windows.Forms.Button();
+            this.space12 = new System.Windows.Forms.Button();
+            this.space14 = new System.Windows.Forms.Button();
+            this.space15 = new System.Windows.Forms.Button();
+            this.space16 = new System.Windows.Forms.Button();
+            this.space17 = new System.Windows.Forms.Button();
+            this.space18 = new System.Windows.Forms.Button();
+            this.space19 = new System.Windows.Forms.Button();
+            this.space20 = new System.Windows.Forms.Button();
+            this.space21 = new System.Windows.Forms.Button();
+            this.space22 = new System.Windows.Forms.Button();
+            this.space23 = new System.Windows.Forms.Button();
+            this.space24 = new System.Windows.Forms.Button();
+            this.space25 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FreeSpace
+            // 
+            this.FreeSpace.Location = new System.Drawing.Point(322, 254);
+            this.FreeSpace.Name = "FreeSpace";
+            this.FreeSpace.Size = new System.Drawing.Size(124, 110);
+            this.FreeSpace.TabIndex = 0;
+            this.FreeSpace.Text = "FREE";
+            this.FreeSpace.UseVisualStyleBackColor = true;
+            this.FreeSpace.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // space1
+            // 
+            this.space1.Location = new System.Drawing.Point(62, 22);
+            this.space1.Name = "space1";
+            this.space1.Size = new System.Drawing.Size(124, 110);
+            this.space1.TabIndex = 1;
+            this.space1.Text = "FREE";
+            this.space1.UseVisualStyleBackColor = true;
+            // 
+            // space2
+            // 
+            this.space2.Location = new System.Drawing.Point(192, 22);
+            this.space2.Name = "space2";
+            this.space2.Size = new System.Drawing.Size(124, 110);
+            this.space2.TabIndex = 2;
+            this.space2.Text = "FREE";
+            this.space2.UseVisualStyleBackColor = true;
+            // 
+            // space3
+            // 
+            this.space3.Location = new System.Drawing.Point(322, 22);
+            this.space3.Name = "space3";
+            this.space3.Size = new System.Drawing.Size(124, 110);
+            this.space3.TabIndex = 3;
+            this.space3.Text = "FREE";
+            this.space3.UseVisualStyleBackColor = true;
+            // 
+            // space4
+            // 
+            this.space4.Location = new System.Drawing.Point(452, 22);
+            this.space4.Name = "space4";
+            this.space4.Size = new System.Drawing.Size(124, 110);
+            this.space4.TabIndex = 4;
+            this.space4.Text = "FREE";
+            this.space4.UseVisualStyleBackColor = true;
+            // 
+            // space5
+            // 
+            this.space5.Location = new System.Drawing.Point(582, 22);
+            this.space5.Name = "space5";
+            this.space5.Size = new System.Drawing.Size(124, 110);
+            this.space5.TabIndex = 5;
+            this.space5.Text = "FREE";
+            this.space5.UseVisualStyleBackColor = true;
+            // 
+            // space6
+            // 
+            this.space6.Location = new System.Drawing.Point(62, 138);
+            this.space6.Name = "space6";
+            this.space6.Size = new System.Drawing.Size(124, 110);
+            this.space6.TabIndex = 6;
+            this.space6.Text = "FREE";
+            this.space6.UseVisualStyleBackColor = true;
+            // 
+            // space7
+            // 
+            this.space7.Location = new System.Drawing.Point(192, 138);
+            this.space7.Name = "space7";
+            this.space7.Size = new System.Drawing.Size(124, 110);
+            this.space7.TabIndex = 7;
+            this.space7.Text = "FREE";
+            this.space7.UseVisualStyleBackColor = true;
+            // 
+            // space8
+            // 
+            this.space8.Location = new System.Drawing.Point(322, 138);
+            this.space8.Name = "space8";
+            this.space8.Size = new System.Drawing.Size(124, 110);
+            this.space8.TabIndex = 8;
+            this.space8.Text = "FREE";
+            this.space8.UseVisualStyleBackColor = true;
+            // 
+            // space9
+            // 
+            this.space9.Location = new System.Drawing.Point(452, 138);
+            this.space9.Name = "space9";
+            this.space9.Size = new System.Drawing.Size(124, 110);
+            this.space9.TabIndex = 9;
+            this.space9.Text = "FREE";
+            this.space9.UseVisualStyleBackColor = true;
+            // 
+            // space10
+            // 
+            this.space10.Location = new System.Drawing.Point(582, 138);
+            this.space10.Name = "space10";
+            this.space10.Size = new System.Drawing.Size(124, 110);
+            this.space10.TabIndex = 10;
+            this.space10.Text = "FREE";
+            this.space10.UseVisualStyleBackColor = true;
+            // 
+            // space11
+            // 
+            this.space11.Location = new System.Drawing.Point(62, 254);
+            this.space11.Name = "space11";
+            this.space11.Size = new System.Drawing.Size(124, 110);
+            this.space11.TabIndex = 11;
+            this.space11.Text = "FREE";
+            this.space11.UseVisualStyleBackColor = true;
+            // 
+            // space12
+            // 
+            this.space12.Location = new System.Drawing.Point(192, 254);
+            this.space12.Name = "space12";
+            this.space12.Size = new System.Drawing.Size(124, 110);
+            this.space12.TabIndex = 12;
+            this.space12.Text = "FREE";
+            this.space12.UseVisualStyleBackColor = true;
+            // 
+            // space14
+            // 
+            this.space14.Location = new System.Drawing.Point(452, 254);
+            this.space14.Name = "space14";
+            this.space14.Size = new System.Drawing.Size(124, 110);
+            this.space14.TabIndex = 13;
+            this.space14.Text = "FREE";
+            this.space14.UseVisualStyleBackColor = true;
+            // 
+            // space15
+            // 
+            this.space15.Location = new System.Drawing.Point(582, 254);
+            this.space15.Name = "space15";
+            this.space15.Size = new System.Drawing.Size(124, 110);
+            this.space15.TabIndex = 14;
+            this.space15.Text = "FREE";
+            this.space15.UseVisualStyleBackColor = true;
+            // 
+            // space16
+            // 
+            this.space16.Location = new System.Drawing.Point(62, 370);
+            this.space16.Name = "space16";
+            this.space16.Size = new System.Drawing.Size(124, 110);
+            this.space16.TabIndex = 15;
+            this.space16.Text = "FREE";
+            this.space16.UseVisualStyleBackColor = true;
+            // 
+            // space17
+            // 
+            this.space17.Location = new System.Drawing.Point(192, 370);
+            this.space17.Name = "space17";
+            this.space17.Size = new System.Drawing.Size(124, 110);
+            this.space17.TabIndex = 16;
+            this.space17.Text = "FREE";
+            this.space17.UseVisualStyleBackColor = true;
+            // 
+            // space18
+            // 
+            this.space18.Location = new System.Drawing.Point(322, 370);
+            this.space18.Name = "space18";
+            this.space18.Size = new System.Drawing.Size(124, 110);
+            this.space18.TabIndex = 17;
+            this.space18.Text = "FREE";
+            this.space18.UseVisualStyleBackColor = true;
+            // 
+            // space19
+            // 
+            this.space19.Location = new System.Drawing.Point(452, 370);
+            this.space19.Name = "space19";
+            this.space19.Size = new System.Drawing.Size(124, 110);
+            this.space19.TabIndex = 18;
+            this.space19.Text = "FREE";
+            this.space19.UseVisualStyleBackColor = true;
+            // 
+            // space20
+            // 
+            this.space20.Location = new System.Drawing.Point(582, 370);
+            this.space20.Name = "space20";
+            this.space20.Size = new System.Drawing.Size(124, 110);
+            this.space20.TabIndex = 19;
+            this.space20.Text = "FREE";
+            this.space20.UseVisualStyleBackColor = true;
+            // 
+            // space21
+            // 
+            this.space21.Location = new System.Drawing.Point(62, 486);
+            this.space21.Name = "space21";
+            this.space21.Size = new System.Drawing.Size(124, 110);
+            this.space21.TabIndex = 20;
+            this.space21.Text = "FREE";
+            this.space21.UseVisualStyleBackColor = true;
+            // 
+            // space22
+            // 
+            this.space22.Location = new System.Drawing.Point(192, 486);
+            this.space22.Name = "space22";
+            this.space22.Size = new System.Drawing.Size(124, 110);
+            this.space22.TabIndex = 21;
+            this.space22.Text = "FREE";
+            this.space22.UseVisualStyleBackColor = true;
+            // 
+            // space23
+            // 
+            this.space23.Location = new System.Drawing.Point(322, 486);
+            this.space23.Name = "space23";
+            this.space23.Size = new System.Drawing.Size(124, 110);
+            this.space23.TabIndex = 22;
+            this.space23.Text = "FREE";
+            this.space23.UseVisualStyleBackColor = true;
+            // 
+            // space24
+            // 
+            this.space24.Location = new System.Drawing.Point(452, 486);
+            this.space24.Name = "space24";
+            this.space24.Size = new System.Drawing.Size(124, 110);
+            this.space24.TabIndex = 23;
+            this.space24.Text = "FREE";
+            this.space24.UseVisualStyleBackColor = true;
+            // 
+            // space25
+            // 
+            this.space25.Location = new System.Drawing.Point(582, 486);
+            this.space25.Name = "space25";
+            this.space25.Size = new System.Drawing.Size(124, 110);
+            this.space25.TabIndex = 24;
+            this.space25.Text = "FREE";
+            this.space25.UseVisualStyleBackColor = true;
+            // 
+            // BotanBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 652);
+            this.Controls.Add(this.space25);
+            this.Controls.Add(this.space24);
+            this.Controls.Add(this.space23);
+            this.Controls.Add(this.space22);
+            this.Controls.Add(this.space21);
+            this.Controls.Add(this.space20);
+            this.Controls.Add(this.space19);
+            this.Controls.Add(this.space18);
+            this.Controls.Add(this.space17);
+            this.Controls.Add(this.space16);
+            this.Controls.Add(this.space15);
+            this.Controls.Add(this.space14);
+            this.Controls.Add(this.space12);
+            this.Controls.Add(this.space11);
+            this.Controls.Add(this.space10);
+            this.Controls.Add(this.space9);
+            this.Controls.Add(this.space8);
+            this.Controls.Add(this.space7);
+            this.Controls.Add(this.space6);
+            this.Controls.Add(this.space5);
+            this.Controls.Add(this.space4);
+            this.Controls.Add(this.space3);
+            this.Controls.Add(this.space2);
+            this.Controls.Add(this.space1);
+            this.Controls.Add(this.FreeSpace);
+            this.Name = "BotanBoard";
+            this.Text = "BotanBoard";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FreeSpace;
+        private System.Windows.Forms.Button space1;
+        private System.Windows.Forms.Button space2;
+        private System.Windows.Forms.Button space3;
+        private System.Windows.Forms.Button space4;
+        private System.Windows.Forms.Button space5;
+        private System.Windows.Forms.Button space6;
+        private System.Windows.Forms.Button space7;
+        private System.Windows.Forms.Button space8;
+        private System.Windows.Forms.Button space9;
+        private System.Windows.Forms.Button space10;
+        private System.Windows.Forms.Button space11;
+        private System.Windows.Forms.Button space12;
+        private System.Windows.Forms.Button space14;
+        private System.Windows.Forms.Button space15;
+        private System.Windows.Forms.Button space16;
+        private System.Windows.Forms.Button space17;
+        private System.Windows.Forms.Button space18;
+        private System.Windows.Forms.Button space19;
+        private System.Windows.Forms.Button space20;
+        private System.Windows.Forms.Button space21;
+        private System.Windows.Forms.Button space22;
+        private System.Windows.Forms.Button space23;
+        private System.Windows.Forms.Button space24;
+        private System.Windows.Forms.Button space25;
+    }
+}
---
+++ BotanBingo/Menu.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace BotanBingo
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(193, 101);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(415, 64);
+            this.Title.TabIndex = 3;
+            this.Title.Text = "BOTAN BINGO";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(308, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 121);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.button1);
+            this.Name = "Menu";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
